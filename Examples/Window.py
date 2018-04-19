ddbd5000a767a31cd456d4cbc34085d19c664259
Niranjan <33854825+niranjanreddy891@users.noreply.github.com>
2018-04-19 11:13:50 +0530

    Update Window.py

--- Examples/Window.py
+++ Examples/Window.py
@@ -1,10 +1,13 @@
+#  @author: niranjanreddy891@gmail.com
+
+
 import tkinter
 
 class Window(tkinter.Tk):
     def __init__(self):
         tkinter.Tk.__init__(self)
         self.title("Window")
-        self.geometry("200x200")
+        self.geometry("400x300")
 
 if __name__ == "__main__":
     application = Window()
