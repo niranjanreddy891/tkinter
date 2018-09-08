bcc2a4b54743387da300dfaf32c758bbb6355338
Niranjan <33854825+niranjanreddy891@users.noreply.github.com>
2018-09-08 22:28:25 +0530

    Update Button.py

--- Examples/Button.py
+++ Examples/Button.py
@@ -5,7 +5,7 @@ import tkinter
 class Button(tkinter.Tk):
     def __init__(self):
         tkinter.Tk.__init__(self)
-        self.title("Button")
+        self.title("Click me!")
 
         button = tkinter.Button(text="Button", command=self.on_button_click)
         button.pack(fill=tkinter.BOTH, expand=1)
